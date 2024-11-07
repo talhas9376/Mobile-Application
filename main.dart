41e9931e07be65da0fee6fd8d00869d81093ceae
talhas9376 <158143604+talhas9376@users.noreply.github.com>
2024-11-07 04:05:03 -0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:talha_new/currency_converter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,10 +6,73 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-  return const MaterialApp(home: CurrencyConverter(),);
-  
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 154, 228, 202),
+          title: const Text('MA Assignment'),
+        ),
+        body: Container(
+          child: Center(
+            child: Container(
+              height: 400,
+              width: 150,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(400),
+              ),
+              child: Container(
+                height: 375,
+                width: 125,
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(400),
+                ),
+                child: Container(
+                  height: 350,
+                  width: 100,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(400),
+                  ),
+                  child: Container(
+                    height: 325,
+                    width: 75,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(400),
+                    ),
+                    child: Container(
+                      height: 300,
+                      width: 50,
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(400),
+                      ),
+                      child: Container(
+                          height: 275,
+                          width: 25,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(400),
+                          )),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
