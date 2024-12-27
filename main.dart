1cfef010451f48e28d6e985d87b081e46ed50e1f
talhas9376 <158143604+talhas9376@users.noreply.github.com>
2024-12-27 16:38:27 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,122 +1,411 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Import the package
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Row(
+    return const MaterialApp(
+      home: PortfolioScreen(),
+    );
+  }
+}
+
+class PortfolioScreen extends StatelessWidget {
+  const PortfolioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Left Column
+              HeaderSection(),
+              InterestSection(),
+              SkillsSection(),
+              ProjectsSection(),
+              EducationSection(),
+              ExperienceSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderSection extends StatelessWidget {
+  const HeaderSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'My PortFolio',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'Muhammad Talha',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'I AM MOBILE APP DEVELOPER AND DESIGNER',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 50),
+          Center(
+            child: Image.asset(
+              'assets/img/profile.jpg',
+            ),
+          ),
+          const SizedBox(height: 50),
+          const Text(
+            'Passionate developer adept at crafting elegant and efficient solutions to complex problems. Dedicated to continuous learning and staying abreast of emerging technologies.',
+            style: TextStyle(fontSize: 14),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          const Row(
+            children: [
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // Title and Description
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Hacker", style: TextStyle(color: Colors.black, fontSize: 24)),
-                          SizedBox(height: 4),
-                          Text(
-                            "Hacking is the act of gaining unauthorized access to a computer system, account, device, or network, achieved by any number of techniques, most of which are cyberattacks. Hackers are those who find and exploit vulnerabilities in systems or networks to compromise them and gain unauthorized access.",
-                            style: TextStyle(color: Colors.green, fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 16),
-
-                    // Icons and Text Row (Phone, Facebook, WhatsApp)
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Column(
-                          children: [
-                            Icon(Icons.phone), // Phone icon
-                            Text("Phone",style: TextStyle(color:Colors.red),),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            FaIcon(FontAwesomeIcons.facebook), // Facebook icon
-                            Text("Facebook",style: TextStyle(color:Colors.blue)),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            FaIcon(FontAwesomeIcons.whatsapp), // WhatsApp icon
-                            Text("WhatsApp",style: TextStyle(color:Colors.green)),
-                          ],
-                        ),
+                        Text('Profession: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text('Software Developer'),
                       ],
                     ),
-                    SizedBox(height: 16),
-
-                    // Centered F, G, X Text
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center, // Center alignment
+                    Row(
                       children: [
-                        Text("F", style: TextStyle(fontSize: 24)),
-                        SizedBox(height: 4),
-                        Text("Text"),
-                        SizedBox(height: 8),
-                        Text("G", style: TextStyle(fontSize: 24)),
-                        SizedBox(height: 4),
-                        Text("Text"),
-                        SizedBox(height: 8),
-                        Text("X", style: TextStyle(fontSize: 24)),
-                        SizedBox(height: 4),
-                        Text("Text"),
+                        Text('Date Of Birth: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text('29-11-2002'),
                       ],
                     ),
-                  ],
-                ),
-              ),
-
-              SizedBox(width: 16),
-
-              // Right Column with Image and Text Description
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Image Placeholder
-                    Container(
-                      width: double.infinity,
-                      height: 150,
-                      child: Image.asset('assets/img.jpg'),
-                    ),
-                    SizedBox(height: 16),
-
-                    // Text Description
-                    Container(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Hacker", style: TextStyle(fontSize: 18)),
-                          SizedBox(height: 4),
-                          Text("Hacking is the act of gaining unauthorized access to a computer system, account, device, or network, achieved by any number of techniques, most of which are cyberattacks. Hackers are those who find and exploit vulnerabilities in systems or networks to compromise them and gain unauthorized access.", style: TextStyle(color:Colors.red,fontSize: 16)),
-                        ],
-                      ),
+                    Row(
+                      children: [
+                        Text('Education: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text('Higher Education'),
+                      ],
                     ),
                   ],
                 ),
               ),
             ],
           ),
+          const SizedBox(height: 16),
+          const Center(
+            child: ElevatedButton(
+              onPressed: null,
+              child: Text('CHECK RESUME'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InterestSection extends StatelessWidget {
+  const InterestSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'My Interests:',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          _buildInterestItem(
+            'Mobile App Development',
+            'Creating innovative and user-friendly mobile applications.',
+          ),
+          _buildInterestItem(
+            'Web Development',
+            'Building responsive and scalable web applications.',
+          ),
+          _buildInterestItem(
+            'Machine Learning',
+            'Exploring algorithms and models to extract insights from data.',
+          ),
+          _buildInterestItem(
+            'UI/UX Design',
+            'Designing intuitive and visually appealing user interfaces.',
+          ),
+          _buildInterestItem(
+            'Cloud Computing',
+            'Leveraging cloud platforms for efficient and scalable solutions.',
+          ),
+          _buildInterestItem(
+            'Open Source Contribution',
+            'Contributing to open source projects and communities.',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInterestItem(String title, String description) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            description,
+            style: const TextStyle(fontSize: 14, color: Colors.black87),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SkillsSection extends StatelessWidget {
+  const SkillsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Skills:',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          _buildSkillItem('Flutter', 0.90),
+          _buildSkillItem('Dart', 0.80),
+          _buildSkillItem('UI/UX Design', 0.50),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSkillItem(String skill, double progress) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            skill,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 8),
+          LinearProgressIndicator(
+            value: progress,
+            backgroundColor: Colors.grey[300],
+            valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+            minHeight: 8,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              '${(progress * 100).toInt()}%',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProjectsSection extends StatelessWidget {
+  const ProjectsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Projects and Experience',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          _buildProjectCard(
+            'Todo App',
+            'A simple todo list app built with Flutter and Firebase.',
+          ),
+          _buildProjectCard(
+            'Weather App',
+            'A weather app built with Flutter that fetches data from a REST API.',
+          ),
+          _buildProjectCard(
+            'Expense Tracker App',
+            'The app enables users to efficiently manage their expenses on-the-go by providing a user-friendly interface to track their spending habits.',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProjectCard(String title, String description) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              description,
+              style: const TextStyle(fontSize: 14),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class EducationSection extends StatelessWidget {
+  const EducationSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Education',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          _buildEducationItem(
+            'Graduation',
+            'The University Of Lahore',
+            'Bachelor of Science in Information Engineering Technology, BSIET (Continue).',
+          ),
+          _buildEducationItem(
+            'Intermediate',
+            'Aspire College',
+            'Fsc. Pre-engineering. (2020-2022)',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEducationItem(String level, String institution, String details) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            level,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            institution,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+          Text(
+            details,
+            style: const TextStyle(fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ExperienceSection extends StatelessWidget {
+  const ExperienceSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Experience',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Front-End Web Developer',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Code Alpha',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'Remote Intern',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Successfully completed internship at Code Alpha Software House, gaining valuable hands-on experience in software development and contributing to real-world projects. Engaged in collaborative teamwork, honed technical skills, and gained insights into the professional software development environment.',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(icon: const Icon(Icons.facebook), onPressed: null),
+              IconButton(icon: Icon(FontAwesomeIcons.instagram), onPressed: null),
+              IconButton(icon: Icon(FontAwesomeIcons.twitter), onPressed: null),
+              IconButton(icon: Icon(Icons.telegram), onPressed: null),
+            ],
+          ),
+        ],
       ),
     );
   }
